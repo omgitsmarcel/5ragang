6efa45518bbd032e70ba4f10df337f7ffb02f84f
omgitsmarcel <46417008+omgitsmarcel@users.noreply.github.com>
2019-01-18 16:25:33 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("529959588154441730")
+setInterval(function() {
+channel.send(`Marcel , Marcel Marcel , Marcel Marcel , Marcel Marcel , Marcel Marcel , Marcel Marcel , Marcel Marcel , Marcel Marcel , Marcel Marcel , Marcel Marcel , Marcel** `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN2);
+client.login(process.env.BOT_TOKEN3);
+client.login(process.env.BOT_TOKEN4);
+client.login(process.env.BOT_TOKEN5);
+client.login(process.env.BOT_TOKEN6);
+client.login(process.env.BOT_TOKEN7);
+client.login(process.env.BOT_TOKEN8);
+client.login(process.env.BOT_TOKEN9);
+client.login(process.env.BOT_TOKEN10);
+client.login(process.env.BOT_TOKEN11);
+client.login(process.env.BOT_TOKEN12);
+client.login(process.env.BOT_TOKEN13);
+client.login(process.env.BOT_TOKEN14);
+client.login(process.env.BOT_TOKEN15);
+client.login(process.env.BOT_TOKEN16);
